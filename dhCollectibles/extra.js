0ce3a1307a5f6d63da3830e4f8f8e71fcda652bd
rociocala <rpciocala@gmail.com>
2023-08-30 22:22:17 -0300

    arreglo

--- dhCollectibles/extra.js
+++ dhCollectibles/extra.js
@@ -1,22 +1,22 @@
 let numeros = [10,20,30,40,50,60,70,80,90,100];
 
-let icero = numeros[0];
-let idos = numeros[2];
-let icuatro = numeros[4]; 
+let[cero, ,dos, ,cuatro, , , , , , ] = numeros;
+let [,uno, ,tres, ,...otros]=numeros;
 
-let otrosNum = [numeros[1],numeros[3],numeros[5],numeros[6],numeros[7],numeros[8],numeros[9],numeros[10]];
-console.log("La posicion cero es: "+icero+",la posicion dos es:"+idos+" y la posicion cuatro es:"+icuatro);
-console.log("El resto de numeros son: "+otrosNum);
+console.log("La posicion cero es: "+cero+",la posicion dos es:"+dos+" y la posicion cuatro es:"+cuatro);
+console.log("El resto de numeros son: "+uno+","+tres+","+otros);
+
+/*--------------------------------------------------------------------------------------------------------------------*/
 
 const mascota = {
-    nombre : 'tincho',
+    nombre : 'Tincho',
     tipo:'perro',
     color:'marron',
     raza:'pitbull'
 }
  
 const{nombre,tipo,color,raza}=mascota
-console.log("Hola les presento a mi mascota su nombre es:"+nombre+ ",es un hermoso "+tipo+",de color:"+color+" y su raza es:"+raza);
+console.log("Hola les presento a mi mascota su nombre es "+nombre+ ",es un hermoso "+tipo+",de color "+color+" y su raza es "+raza);
 
 
 
